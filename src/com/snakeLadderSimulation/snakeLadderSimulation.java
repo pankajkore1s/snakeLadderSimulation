202f37acb444dd3801721dcc9bfde1461b7f4c66
pankaj kore <korep95@gmail.com>
2022-01-23 21:44:04 +0530

    [pankaj]added rolled a dice to get number

--- src/com/snakeLadderSimulation/snakeLadderSimulation.java
+++ src/com/snakeLadderSimulation/snakeLadderSimulation.java
@@ -1,11 +1,21 @@
 package com.snakeLadderSimulation;
 
 public class snakeLadderSimulation {
+	
+	public static boolean isPlayer=true;
+	public static final int winPosition=100;
+	
 	public static void main (String []args) {
 
 		int initialPosition=0;
+		int numberOfDice=0;
+		int playerAPosition=0;
+		int playerBPosition=0;
 		System.out.println("Welcome to SnakeLadder Simulation");
 
+		int dice=(int)(Math.random()*6)+1;
+		numberOfDice++;
+		System.out.println("Number got on dice=" +dice);
 	} 
 
 }
