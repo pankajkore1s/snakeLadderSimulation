4140bdffbcb0a9644cec67ad6a9a4be9417254f5
pankaj kore <korep95@gmail.com>
2022-01-23 21:33:11 +0530

    [pankaj]added start position 0

--- src/com/snakeLadderSimulation/snakeLadderSimulation.java
+++ src/com/snakeLadderSimulation/snakeLadderSimulation.java
@@ -1,5 +1,11 @@
 package com.snakeLadderSimulation;
 
 public class snakeLadderSimulation {
+	public static void main (String []args) {
+
+		int initialPosition=0;
+		System.out.println("Welcome to SnakeLadder Simulation");
+
+	} 
 
 }
